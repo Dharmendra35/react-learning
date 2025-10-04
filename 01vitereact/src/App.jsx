22002b1ffa987b53a56acacb32d1f48c40fd14db
Dharmendra35 <dk292767@gmail.com>
2025-10-04 10:27:41 +0530

    creation of own react library and jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,5 +1,15 @@
+import Chai from "./chai";
+
 function App() {
-  return <h1>This is a vite project</h1>;
+  const username = "mai aur tum";
+  //this variable is directly injected in any tag using {}, and that will final evaluated expression, evaluated expression not work as variable, we can,t use that as js varibale after
+  return (
+    <>
+      <Chai></Chai>
+      <h1>This is a vite project {username}</h1>
+      <p>test para</p>
+    </>
+  );
 }
 
 export default App;
